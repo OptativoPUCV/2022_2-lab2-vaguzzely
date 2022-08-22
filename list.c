84eec52a1783c266535744b81694ad5235876322
Replit user <>
2022-08-22 01:36:41 +0000

    new try

--- list.c
+++ list.c
@@ -53,11 +53,12 @@ void * nextList(List * list) {
 
 void * lastList(List * list) {
   list->current = list->tail; 
-    return list->current->data;
+    return (list->current->data);
 }
 /* La segunda función retorna el dato del nodo anterior a current y actualiza el current para que apunte a ese nodo.*/
 void * prevList(List * list) {
-    return NULL;
+  list->current = list->current->prev;
+    return (list->current->data);
 }
 
 void pushFront(List * list, void * data){
